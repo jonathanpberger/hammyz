152632a53f0d5037e51feda944168459979d9502
Jonathan Berger <pair+jonathanpberger@pivotallabs.com>
2013-08-04 15:33:21 -0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hammyz::Application.config.secret_token = 'a3e04598694a80b9907cb80cfeeff8d33b3a6f0562f6ccdccab5c0fb34235c54d51fb74c31649abcc441000d86746e335659236b7a401e9713e9dddf0364a8eb'
